3206df7253ea59e91246d75c2dc1288daeaca8ef
wembley84 <wembleymariano47@gmail.com>
2023-05-31 20:41:34 -0300

    desafio virtual-fone

---
+++ Forms/desafio/virtual-fone/main.js
@@ -0,0 +1,13 @@
+function valorPrimeirobotao(){
+    alert(document.querySelector
+        ('input[type=buton]').value;
+}
+
+const listaTeclas = document.querySelectorAll
+('input[type=buton]');
+
+Function valorBotonUm(){
+    alert(listaTeclas[0].value);
+
+}
+listaTeclas[0].onclick = valorBotonUm
