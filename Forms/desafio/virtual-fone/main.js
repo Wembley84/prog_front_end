b06a52acccd510e1889fb0844f3690888610fcab
wembley84 <wembleymariano84@gmail.com>
2023-06-06 22:21:45 -0300

    virtual fone teclas funcionando

--- Forms/desafio/virtual-fone/main.js
+++ Forms/desafio/virtual-fone/main.js
@@ -1,13 +1,41 @@
-function valorPrimeirobotao(){
-    alert(document.querySelector
-        ('input[type=buton]').value;
+const listaTeclas = document.querySelectorAll('input[type=button]');
+const display =  document.querySelector('input[type=tel]');
+
+// function valorBotaoUmJanela (){ // mostra o valor do primeiro botão
+//     valorBotaoUm(listaTeclas[0].value);
+// }
+// function valorBotaoUm(){
+
+//     display.value = display.value + listaTeclas[0].value;
+// }
+// listaTeclas[0].onclick = valorBotaoUm; // chama a função quando clicado
+
+function valorBotao(valor){
+    display.value = display.value + listaTeclas[valor] .value
+    display.value = display.value + listaTeclas[valor].value;
 }
 
-const listaTeclas = document.querySelectorAll
-('input[type=buton]');
-
-Function valorBotonUm(){
-    alert(listaTeclas[0].value);
-
+function limpaDisplay(){
+    display.value = '' // coloca o valor do display como nulo
+    display.value = '' // coloca o valor do display como vazio
 }
-listaTeclas[0].onclick = valorBotonUm
+
+listaTeclas[0].onclick = function(){
+    valorBotao(11);
+} 
+// let i = 0;
+// while (i < listaTeclas.length){
+//     const tecla = listaTeclas[i];
+//     const indice = i
+//     tecla.onclick = function(){
+//         valorBotao(indice);     
+//     }
+//     i++;
+// }
+
+for (let i = 0; i < listaTeclas.length; i++){
+        const tecla = listaTeclas[i];
+        tecla.onclick = function(){
+        valorBotao(i);     
+    }
+}
