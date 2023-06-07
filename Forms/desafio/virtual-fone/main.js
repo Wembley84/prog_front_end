d5a1e1c7ee2feec389b1f5baba39f3075b936f1b
wembley84 <wembleymariano47@gmail.com>
2023-06-07 19:47:47 -0300

    condicional

--- Forms/desafio/virtual-fone/main.js
+++ Forms/desafio/virtual-fone/main.js
@@ -1,41 +1,35 @@
-const listaTeclas = document.querySelectorAll('input[type=button]');
-const display =  document.querySelector('input[type=tel]');
-
-// function valorBotaoUmJanela (){ // mostra o valor do primeiro botão
-//     valorBotaoUm(listaTeclas[0].value);
-// }
-// function valorBotaoUm(){
-
-//     display.value = display.value + listaTeclas[0].value;
-// }
-// listaTeclas[0].onclick = valorBotaoUm; // chama a função quando clicado
-
-function valorBotao(valor){
-    display.value = display.value + listaTeclas[valor] .value
-    display.value = display.value + listaTeclas[valor].value;
-}
-
-function limpaDisplay(){
-    display.value = '' // coloca o valor do display como nulo
-    display.value = '' // coloca o valor do display como vazio
-}
-
-listaTeclas[0].onclick = function(){
-    valorBotao(11);
-} 
-// let i = 0;
-// while (i < listaTeclas.length){
-//     const tecla = listaTeclas[i];
-//     const indice = i
-//     tecla.onclick = function(){
-//         valorBotao(indice);     
-//     }
-//     i++;
-// }
-
-for (let i = 0; i < listaTeclas.length; i++){
-        const tecla = listaTeclas[i];
-        tecla.onclick = function(){
-        valorBotao(i);     
+function valorPrimeiroBotao(){
+    function valorPrimeiroBotao() {
+        alert(document.querySelector
+            ('input[type=button]').value);
     }
 }
+    
+    const listaTeclas = document.querySelectorAll
+    ('input[type=button]');
+        ('input[type=button]');
+    const display = document.querySelector('input[type=tel]');
+    
+    function valorBotaoUmJanela(){
+    function valorBotaoUmJanela() {
+        alert(listaTeclas[0].value);
+    }
+}   
+    function valorBotaoUm(){
+    function valorBotaoUm() {
+        display.value += listaTeclas[0].value;
+    }
+}   
+    function valorBotao(valor){
+       display.value += listaTeclas[valor].value;
+    function valorBotao(valor) {
+        if (display.value.length < 8 && listaTeclas[valor].value !== '#') {
+            display.value += listaTeclas[valor].value;
+        } else {
+            alert('Fora do limite permitido ou função não liberada.')
+        }
+    
+    }
+    
+    
+    }
